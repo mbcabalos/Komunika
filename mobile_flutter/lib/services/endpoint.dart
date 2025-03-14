541e241c44eedb7362c78b8c0085fcc515829beb
SweetLanaSison <sweetlanasison@gmail.com>
2025-03-14 18:52:19 +0800

    fb, email

--- mobile_flutter/lib/services/endpoint.dart
+++ mobile_flutter/lib/services/endpoint.dart
@@ -1,6 +1,7 @@
 class Endpoint {
   // static const String url = "http://192.168.254.111:5000";
   static const String url = "http://192.168.254.102:5000"; // David
+  //static const String url = "http://192.168.199.115:5000"; // Lana
   // static const String url = "http://172.20.10.2:5000"; // Benedict
   // static const String url = "http://192.168.1.133:5000"; // Benedict
   static const String baseUrl = "$url/api";
